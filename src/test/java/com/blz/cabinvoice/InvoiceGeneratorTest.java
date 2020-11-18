69f2656e3dbbd419a9085f035e274b7944260aa8
codexakshay <codexakshay@gmail.com>
2020-11-18 20:57:53 +0530

    CabInvoiceGenerator : Calculate Fare

---
+++ src/test/java/com/blz/cabinvoice/InvoiceGeneratorTest.java
@@ -0,0 +1,31 @@
+package com.blz.cabinvoice;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class InvoiceGeneratorTest {
+
+	static InvoiceGenerator invoiceGenerator;
+
+	@BeforeClass
+	public static void Obj() {
+		invoiceGenerator = new InvoiceGenerator();
+	}
+
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calaculateFare(distance, time);
+		Assert.assertEquals(25, fare, 0.0);
+	}
+
+	@Test
+	public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calaculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
+}
